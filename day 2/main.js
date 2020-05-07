c384372045613d38e3e14b22c711f202aa87d4c7
Captian_Levi1999 <50689422+Raviteja1999@users.noreply.github.com>
2020-05-07 20:22:00 +0530

    first commit

---
+++ day 2/main.js
@@ -0,0 +1,10 @@
+//variables:- var, let, const
+
+//modes: ''(normal), strict mode
+////scope  ,define, decalre of variables
+// strict mode can be activated by -> 'use strict' before the starting of the program 
+// in normal mode is defaulty takes the variable as var type
+//scope- var can be used for the function(function scoped) , let - is block scoped for the particular {-start  to }-end
+//define and decalare- var i // declare//=10; //define  var are redeclareable and redefinable
+//let is redefinable but not redeclarable
+//constant is block scoped it is not redefinable and non declarable
