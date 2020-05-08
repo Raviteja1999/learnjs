b6abb62f69684f9ca8b57774ff33966dcabc5a30
Captian_Levi1999 <50689422+Raviteja1999@users.noreply.github.com>
2020-05-08 20:46:50 +0530

    day2 test case

---
+++ day 2/testcase_variables.js
@@ -0,0 +1,29 @@
+
+
+
+
+main();
+
+function main(){
+var a=19;
+for(var a=1;a<8;a++)
+{
+    console.log(a);
+}
+console.log(a);
+}
+
+// main2();
+
+//     function main(){
+// let a=29;
+// for(let a=1;a<8;a++)
+// {
+//     console.log(a); //
+// }                              
+// console.log(a);          //result : 19
+// }
+
+
+
+
