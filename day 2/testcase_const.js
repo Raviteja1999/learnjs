589aaf721b2756c8f06d9324ebad4e1e4c892c66
Captian_Levi1999 <50689422+Raviteja1999@users.noreply.github.com>
2020-05-09 12:12:51 +0530

    day 2 testcase Constant

---
+++ day 2/testcase_const.js
@@ -0,0 +1,21 @@
+main();
+//Scope of Varible const
+function main() {
+    // const i = 2;
+    // for ( a = 0; a < 5; a++) {     // a constant variable cannot be redefined or redeclared
+    //  i=a;
+    //  console.log(i);
+     
+    // }
+    // console.log(a);
+
+     const name = 'iGurkul';
+     name = 'teja';
+    
+     console.log(name);
+    
+} 
+
+
+
+
